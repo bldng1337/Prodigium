72657faa35c2442c8c9c7f2a779b15a016f950fd
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2020-10-15 00:39:38 +0200

    Fixed the BufferCreation

--- Engine/src/me/engine/Utils/Renderer.java
+++ Engine/src/me/engine/Utils/Renderer.java
@@ -1,6 +1,7 @@
 package me.engine.Utils;
 
 import java.io.File;
+import java.util.Arrays;
 
 import org.lwjgl.opengl.GL45;
 
@@ -8,8 +9,8 @@ import me.engine.Main;
 
 public class Renderer {
 
-	public static final int MAXDRAW=500;
-	public static final int MAXCALLS=10;
+	public static final int MAXDRAW=6000;
+	public static final int MAXCALLS=60;
 	float[] vertecies;
 	int vindex=0,vbindex;
 	private VertexBuffer[] v;
@@ -19,31 +20,30 @@ public class Renderer {
 		vbindex=-1;
 	}
 	
-	
 	public void renderQuad(float x, float y, float width, float height, long texid) {
 		vertecies[vindex++]=x;
 		vertecies[vindex++]=y+height;
-		vertecies[vindex++]=0;
+		vertecies[vindex++]=1;
 		
 		vertecies[vindex++]=x;
 		vertecies[vindex++]=y;
-		vertecies[vindex++]=0;
+		vertecies[vindex++]=1;
 		
 		vertecies[vindex++]=x+width;
 		vertecies[vindex++]=y;
-		vertecies[vindex++]=0;
-		
-		vertecies[vindex++]=x+width;
-		vertecies[vindex++]=y;
-		vertecies[vindex++]=0;
-		
-		vertecies[vindex++]=x+width;
-		vertecies[vindex++]=y+height;
-		vertecies[vindex++]=0;
+		vertecies[vindex++]=1;
 		
 		vertecies[vindex++]=x;
 		vertecies[vindex++]=y+height;
-		vertecies[vindex++]=0;
+		vertecies[vindex++]=1;
+		
+		vertecies[vindex++]=x+width;
+		vertecies[vindex++]=y+height;
+		vertecies[vindex++]=1;
+		
+		vertecies[vindex++]=x+width;
+		vertecies[vindex++]=y;
+		vertecies[vindex++]=1;
 		
 		if(vindex>MAXDRAW-18)
 			flush();
@@ -53,28 +53,34 @@ public class Renderer {
 		if(vindex<18)
 			return;
 		vbindex++;
+		if(vbindex>=MAXCALLS) {
+			Main.log.severe("Rendered to much triangles ");
+			vbindex=-1;
+			return;
+		}
 		if(v[vbindex]==null) {
 			v[vbindex]=new VertexBuffer(false);
-			v[vbindex].createBuffer(vertecies, 0, 3);
+			v[vbindex].createBuffer(Arrays.copyOfRange(vertecies, 0,vindex), 0, 3);
 		}else {
-			v[vbindex].updateBuffer(vertecies, 0, 3);
+			v[vbindex].updateBuffer(Arrays.copyOfRange(vertecies, 0,vindex), 0, 3);
 		}
 		vindex=0;
 	}
+	
 	Shader s=new Shader(new File(Main.dir.getAbsolutePath()+"\\Assets\\Shader\\std.frag"), new File(Main.dir.getAbsolutePath()+"\\Assets\\Shader\\std.vert"));
 	public void render() {
 		flush();
 		vindex=0;
-		vbindex=-1;
-		GlStateManager.bindShader(s);
-		for(int i=0;i<vbindex;i++) {
+		for(int i=0;i<=vbindex;i++) {
 			VertexBuffer vb=v[i];
 			vb.bind(0);
 			GL45.glDrawArrays(GL45.GL_TRIANGLES, 0, vb.getbuffersize(0));
 			vb.unbind();
 		}
-		GlStateManager.unbindShader();
-		vbindex=0;
+	}
+	
+	public void clear() {
+		vbindex=-1;
 	}
 	
 	
