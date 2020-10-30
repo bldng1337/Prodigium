e65efd462f187e0643d01c28727d514e7824af3c
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2020-10-30 17:04:52 +0100

    Changed Project Structure
    Removed Files not needet anymore

--- Engine/Assets/Scripts/Test/Test.js
+++
@@ -1,9 +0,0 @@
-var Animation = Java.type("me.engine.Entity.Animation");
-
-function update(){
-	if(e.motionX!=0||e.motionY!=0){
-		e.currTexture=Animation.RUNNING;
-	}else{
-		e.currTexture=Animation.IDLE;
-	}
-}
