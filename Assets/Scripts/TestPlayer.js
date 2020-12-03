7afb59c391e46a980e6a58aa73b0c6ff0a4364b9
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2020-12-03 09:20:12 +0100

    Fixed Errors

--- Assets/Scripts/TestPlayer.js
+++ Assets/Scripts/TestPlayer.js
@@ -1,9 +1,19 @@
 var Animation = Java.type("me.engine.Entity.Animation");
 
 function update(){
-	if(e.motionX!=0||e.motionY!=0){
-		e.currTexture=Animation.RUNNING;
-	}else{
-		e.currTexture=Animation.IDLE;
+	if(e.getAnimation()==Animation.DEATH){
+		return;
 	}
+	if(e.health<=0){
+		e.setAnimation(Animation.DEATH);
+		return;
+	}
+	if(e.currTexture!=Animation.ATTACKING){
+		if(e.motionX!=0||e.motionY!=0){
+			e.currTexture=Animation.RUNNING;
+		}else{
+			e.currTexture=Animation.IDLE;
+		}
+	}
+	
 }
